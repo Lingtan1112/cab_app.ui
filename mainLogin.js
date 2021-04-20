ad346ae79c4f93ac1c12dd7476a15f62ace77e0e
Lingtan1112 <77401106+Lingtan1112@users.noreply.github.com>
2021-04-20 23:54:37 +0530

    Create mainLogin.js

---
+++ mainLogin.js
@@ -0,0 +1,18 @@
+function validate(){
+  event.preventDefault();
+  var user=document.getElementById("userName").value;
+  var pwd = document.getElementById("password").value;
+  let wrong =document.getElementById("invalid");
+  if((user==9600923846)&&(pwd=="Lingtan")){
+    alert("Successfully logged in");
+    window.location="afterLogin.html";
+  }
+  else{
+    wrong.style.visibility = "visible";
+    
+}
+return false;
+}
+//Onsubmit takes the contents of the form to the next page, 
+//the onclick button does not take the values to the next page values has to taken explicitly
+//The Resetbutton resets the content of the form
